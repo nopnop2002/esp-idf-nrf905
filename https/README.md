d489ec7744081e1566954525de3186a02e02ca1f
nopnop2002 <>
2026-08-14 18:13:48 +0900

    Added https project

---
+++ https/README.md
@@ -0,0 +1,50 @@
+# HTTPS Example   
+This is nrf905 and HTTPS gateway application.   
+Receive from cc1101 and send to HTTPS Server.   
+ESP32 acts as HTTPS Client.   
+
+```
+            +-----------+           +-----------+            +------------+
+            |           |           |           |            |            |
+==(Radio)==>|  nRF905   |--(SPI)--->|   ESP32   |--(HTTPS)-->|HTTPS Server|
+            |           |           |           |            |            |
+            +-----------+           +-----------+            +------------+
+```
+
+# Installation
+```
+git clone https://github.com/nopnop2002/esp-idf-nrf905
+cd esp-idf-nrf905/https/
+chmod 777 mkkey.sh
+./mkkey.sh
+idf.py menuconfig
+idf.py flash
+```
+
+mkkey.sh creates a server certificate file.   
+The server certificate file is associated with the HTTPS server's IP address.   
+mkkey.sh automatically retrieves the IP address of the server on which the script is executed and treats that address as an HTTPS server.   
+To manually configure the HTTPS server's IP address, modify the script as follows:   
+```
+IP="192.168.0.123"
+openssl req -x509 -new -nodes -key server.key -subj "/CN=${IP}" -days 10000 -out server.crt
+```
+
+
+# Configuration
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/7541ebac-ba46-4ee5-aa81-157af2ead508" />
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/fcb320c7-4921-4a76-bef3-c34318a5a2a1" />
+
+## WiFi Setting
+Set the information of your access point.   
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/198be139-48bf-4643-b85c-316add820297" />
+
+## Server Setting
+Set the information of your HTTPS server.   
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/1ee2a0de-a7ad-480f-8028-ae165b14f145" />
+
+# Start the HTTPS server
+```
+python3 https-server.py
+```
+<img width="659" height="486" alt="Image" src="https://github.com/user-attachments/assets/48e78bdf-5b53-4729-8d5a-1a4a6d0a47f1" />
---
+++ https/main/main.c
@@ -0,0 +1,261 @@
+/* The example of nRF905
+ *
+ * This sample code is in the public domain.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/message_buffer.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "mdns.h"
+
+#include "nRF905.h"
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "MAIN";
+
+static int s_retry_num = 0;
+
+MessageBufferHandle_t xMessageBufferTrans;
+MessageBufferHandle_t xMessageBufferRecv;
+
+// The total number of bytes (not single messages) the message buffer will be able to hold at any one time.
+size_t xBufferSizeBytes = 1024;
+// The size, in bytes, required to hold each item in the message,
+size_t xItemSize = NRF905_MAX_PAYLOAD;
+
+#define RXADDR 0xE7E7E7E7 // Address of this device
+#define TXADDR 0xE7E7E7E7 // Address of device to send to
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+		esp_wifi_connect();
+	} else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+			esp_wifi_connect();
+			s_retry_num++;
+			ESP_LOGI(TAG, "retry to connect to the AP");
+		} else {
+			xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+		}
+		ESP_LOGI(TAG,"connect to the AP fail");
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+		ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+		s_retry_num = 0;
+		xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+	}
+}
+
+esp_err_t wifi_init_sta(void)
+{
+	s_wifi_event_group = xEventGroupCreate();
+
+	ESP_ERROR_CHECK(esp_netif_init());
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_create_default_wifi_sta();
+
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	esp_event_handler_instance_t instance_any_id;
+	esp_event_handler_instance_t instance_got_ip;
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+		ESP_EVENT_ANY_ID,
+		&event_handler,
+		NULL,
+		&instance_any_id));
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+		IP_EVENT_STA_GOT_IP,
+		&event_handler,
+		NULL,
+		&instance_got_ip));
+
+	wifi_config_t wifi_config = {
+		.sta = {
+			.ssid = CONFIG_ESP_WIFI_SSID,
+			.password = CONFIG_ESP_WIFI_PASSWORD,
+			/* Setting a password implies station will connect to all security modes including WEP/WPA.
+			 * However these modes are deprecated and not advisable to be used. Incase your Access point
+			 * doesn't support WPA2, these mode can be enabled by commenting below line */
+			.threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+			.pmf_cfg = {
+				.capable = true,
+				.required = false
+			},
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	/* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+	 * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+	esp_err_t ret_value = ESP_OK;
+	EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+		WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+		pdFALSE,
+		pdFALSE,
+		portMAX_DELAY);
+
+	/* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+	 * happened. */
+	if (bits & WIFI_CONNECTED_BIT) {
+		ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+	} else if (bits & WIFI_FAIL_BIT) {
+		ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s", CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+		ret_value = ESP_FAIL;
+	} else {
+		ESP_LOGE(TAG, "UNEXPECTED EVENT");
+		ret_value = ESP_FAIL;
+	}
+
+	/* The event will not be processed after unregister */
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+	ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+	vEventGroupDelete(s_wifi_event_group);
+	return ret_value;
+}
+
+esp_err_t query_mdns_host(const char * host_name, char *ip)
+{
+	ESP_LOGD(__FUNCTION__, "Query A: %s", host_name);
+
+	struct esp_ip4_addr addr;
+	addr.addr = 0;
+
+	esp_err_t err = mdns_query_a(host_name, 10000,	&addr);
+	if(err){
+		if(err == ESP_ERR_NOT_FOUND){
+			ESP_LOGW(__FUNCTION__, "%s: Host was not found!", host_name);
+		} else {
+			ESP_LOGE(__FUNCTION__, "Query Failed: %s", esp_err_to_name(err));
+		}
+		return ESP_FAIL;
+	}
+
+	ESP_LOGD(__FUNCTION__, "Query A: %s.local resolved to: " IPSTR, host_name, IP2STR(&addr));
+	sprintf(ip, IPSTR, IP2STR(&addr));
+	return ESP_OK;
+}
+
+void convert_mdns_host(char * from, char * to)
+{
+	ESP_LOGI(__FUNCTION__, "from=[%s]",from);
+	strcpy(to, from);
+	char *sp;
+	sp = strstr(from, ".local");
+	if (sp == NULL) return;
+
+	int _len = sp - from;
+	ESP_LOGD(__FUNCTION__, "_len=%d", _len);
+	char _from[128];
+	strcpy(_from, from);
+	_from[_len] = 0;
+	ESP_LOGI(__FUNCTION__, "_from=[%s]", _from);
+
+	char _ip[128];
+	esp_err_t ret = query_mdns_host(_from, _ip);
+	ESP_LOGI(__FUNCTION__, "query_mdns_host=%d _ip=[%s]", ret, _ip);
+	if (ret != ESP_OK) return;
+
+	strcpy(to, _ip);
+	ESP_LOGI(__FUNCTION__, "to=[%s]", to);
+}
+
+void rx_task(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+
+	// Set address of this device
+	nRF905_setListenAddress(RXADDR);
+
+	// Put into receive mode
+	nRF905_RX();
+
+	uint8_t buffer[xItemSize];
+
+	while(1) {
+		uint8_t packetStatus = nRF905_poll();
+		if (packetStatus == NRF905_RX_INVALID) {
+			ESP_LOGW(pcTaskGetName(NULL), "Invalid packet!");
+			nRF905_RX();
+		} else if (packetStatus == NRF905_ADDR_MATCH) {
+			ESP_LOGI(pcTaskGetName(NULL), "Address match!");
+		} else if (packetStatus == NRF905_RX_COMPLETE) {
+			ESP_LOGI(pcTaskGetName(NULL), "Got packet!");
+			// Read payload
+			nRF905_read(buffer, sizeof(buffer));
+			// Show received data
+			ESP_LOG_BUFFER_HEXDUMP(pcTaskGetName(NULL), buffer, sizeof(buffer), ESP_LOG_INFO);
+			ESP_LOGI(pcTaskGetName(NULL), "%s", buffer);
+
+			int rxLen = strlen((char *)buffer);
+			ESP_LOGI(pcTaskGetName(NULL), "rxLen=%d", rxLen);
+			size_t spacesAvailable = xMessageBufferSpacesAvailable( xMessageBufferTrans );
+			ESP_LOGI(pcTaskGetName(NULL), "spacesAvailable=%d", spacesAvailable);
+			size_t sended = xMessageBufferSend(xMessageBufferTrans, buffer, rxLen, 100);
+			if (sended != rxLen) {
+				ESP_LOGE(pcTaskGetName(NULL), "xMessageBufferSend fail rxLen=%d sended=%d", rxLen, sended);
+				break;
+			}
+		}
+		vTaskDelay(1); // Avoid Watchdog asserts
+	} // end while
+
+	vTaskDelete(NULL);
+}
+
+void https_client(void *pvParameters);
+	
+void app_main()
+{
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// Initialize WiFi
+	ESP_ERROR_CHECK(wifi_init_sta());
+
+	// Create MessageBuffer
+	xMessageBufferTrans = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferTrans );
+	xMessageBufferRecv = xMessageBufferCreate(xBufferSizeBytes);
+	configASSERT( xMessageBufferRecv );
+
+	// Initialize mDNS
+    ESP_ERROR_CHECK( mdns_init() );
+
+	// Initialize nRF905
+	nRF905_begin();
+	nRF905_setChannel(CONFIG_RF69_CHANNEL);
+	nRF905_printConfig();
+
+	xTaskCreate(&rx_task, "RX", 1024*3, NULL, 5, NULL);
+	xTaskCreate(&https_client, "HTTP_CLIENT", 1024*4, NULL, 5, NULL);
+}
+
